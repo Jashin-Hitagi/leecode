587f9bcafa07135b8b074f71b6c61d770a8e7609
Jashin <wh775349543@gmail.com>
2021-12-15 14:06:58 +0800

    update

--- src/main/java/com/example/demo/leetcode/editor/cn/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/example/demo/leetcode/editor/cn/LongestSubstringWithoutRepeatingCharacters.java
@@ -41,7 +41,7 @@ package com.example.demo.leetcode.editor.cn;
 // 提示： 
 //
 // 
-// 0 <= s.length <= 5 * 10⁴ 
+// 0 <= s.length <= 5 * 10⁴
 // s 由英文字母、数字、符号和空格组成 
 // 
 // Related Topics 哈希表 字符串 滑动窗口 👍 6396 👎 0
@@ -49,14 +49,81 @@ package com.example.demo.leetcode.editor.cn;
 public class LongestSubstringWithoutRepeatingCharacters{
     public static void main(String[] args) {
         Solution solution = new LongestSubstringWithoutRepeatingCharacters().new Solution();
-        
+        System.out.println(solution.lengthOfLongestSubstring("pwwkew"));
     }
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+        //暴力解法
+//    public int lengthOfLongestSubstring(String s) {
+//        if (s == "" || s == null){
+//            return 0;
+//        }
+//        //存放不重复的子串
+//        String child = "";
+//        //最长的不重复子串
+//        String res = "";
+//        for (int i = 0; i < s.length(); i++){
+//            String a = String.valueOf(s.charAt(i));
+//            if (!child.contains(a)){
+//                child += a;
+//                if (i == s.length() -1 && child.length() > res.length()){
+//                    res = child;
+//                }
+//            }else {
+//                if (child.length() > res.length()){
+//                    res = child;
+//                }
+//                child = child.substring(child.lastIndexOf(a)+1) + a;
+//            }
+//        }
+//        return res.length();
+//    }
+
+    //滑动窗口
+//    public int lengthOfLongestSubstring(String s) {
+//        int n = s.length(), ans = 0;
+//        Map<Character, Integer> map = new HashMap<>();
+//        for (int end = 0, start = 0; end < n; end++) {
+//            char alpha = s.charAt(end);
+//            if (map.containsKey(alpha)) {
+//                start = Math.max(map.get(alpha), start);
+//            }
+//            ans = Math.max(ans, end - start + 1);
+//            map.put(s.charAt(end), end + 1);
+//        }
+//        return ans;
+//    }
+
     public int lengthOfLongestSubstring(String s) {
-        return 0;
+        //字符串下标
+        int i = 0;
+        //重复字符下标 + 1
+        int flag = 0;
+        //不重复子串长度
+        int length = 0;
+        int result = 0;
+        while (i < s.length()) {
+            //从重复字符开始查找该字符位置
+            int pos = s.indexOf(s.charAt(i),flag);
+            //如果字符重复
+            if (pos < i) {
+                if (length > result) {
+                    result = length;
+                }
+                if (result >= s.length() - pos - 1) {
+                    return result;
+                }
+                length = i - pos - 1;
+                flag = pos + 1;
+            }
+            length++;
+            i++;
+        }
+        return length;
     }
+
+
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
